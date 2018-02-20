c62688b84ef9f5edc00ee518b7f4fc4c8a0a6279
Leon K <leonknight@outlook.com>
2018-02-20 03:58:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function happyHolidayto (holiday, name)
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown (days, holiday)
+function holidayCountdown (holiday, days)
 {
   return `It's ${days} days until ${holiday}!`
 }
